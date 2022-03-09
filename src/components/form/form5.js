6244afebc86ab6a429dffafb7a04e748cbae317e
MUKUNDA <mukundakamatham@gmail.com>
2022-03-09 10:36:36 +0000

    update

--- src/components/form/form5.js
+++ src/components/form/form5.js
@@ -38,34 +38,34 @@ const StepFive = ({ nextStep, handleFormData1, prevStep, values }) => {
             <ButtonToolbar aria-label="Toolbar with button groups">
   
   <ButtonGroup value="1"   onClick={handleFormData1("1")} className="me-2 ms-5" aria-label="Third group">
-    <Button>1</Button>
+    <Button variant={values.fitness_level==="1"?"primary":"outline-primary"}>1</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("2")}  value="2" className="me-2" aria-label="Third group">
-    <Button>2</Button>
+    <Button variant={values.fitness_level==="2"?"primary":"outline-primary"}>2</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("3")}  value="3" className="me-2"  aria-label="Third group">
-    <Button>3</Button>
+    <Button variant={values.fitness_level==="3"?"primary":"outline-primary"}>3</Button>
   </ButtonGroup>
   <ButtonGroup  onClick={handleFormData1("4")} value="4" className="me-2"  aria-label="Third group">
-    <Button>4</Button>
+    <Button variant={values.fitness_level==="4"?"primary":"outline-primary"}>4</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("5")}  value="5" className="me-2"  aria-label="Third group">
-    <Button>5</Button>
+    <Button variant={values.fitness_level==="5"?"primary":"outline-primary"}>5</Button>
   </ButtonGroup>
   <ButtonGroup  onClick={handleFormData1("6")} value="6" className="me-2"  aria-label="Third group">
-    <Button>6</Button>
+    <Button variant={values.fitness_level==="6"?"primary":"outline-primary"}>6</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("7")}  value="7" className="me-2"  aria-label="Third group">
-    <Button>7</Button>
+    <Button variant={values.fitness_level==="7"?"primary":"outline-primary"}>7</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("8")} value="8" className="me-2"  aria-label="Third group">
-    <Button>8</Button>
+    <Button variant={values.fitness_level==="8"?"primary":"outline-primary"}>8</Button>
   </ButtonGroup>
   <ButtonGroup  onClick={handleFormData1("9")} value="9" className="me-2"  aria-label="Third group">
-    <Button>9</Button>
+    <Button variant={values.fitness_level==="9"?"primary":"outline-primary"}>9</Button>
   </ButtonGroup>
   <ButtonGroup onClick={handleFormData1("10")}  value="10" className="me-2"  aria-label="Third group">
-    <Button>10</Button>
+    <Button variant={values.fitness_level==="10"?"primary":"outline-primary"}>10</Button>
   </ButtonGroup>
 </ButtonToolbar>
             </Form.Group>
